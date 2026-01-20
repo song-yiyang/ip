c5b3368c1ed2c1841761b20c2569094b533a0083
Song Yiyang <E1456773@u.nus.edu>
2026-01-20 17:06:20 +0800

    Add Level-0

---
+++ src/main/java/Socket.java
@@ -0,0 +1,13 @@
+public class Socket {
+    public static void main(String[] args) {
+        String logo = """
+                ____________________________________________________________
+                 Hello! I'm Socket
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(logo);
+    }
+}
