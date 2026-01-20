e123809a17edd9e4c53041ed5e826c855b123fb5
Song Yiyang <E1456773@u.nus.edu>
2026-01-20 20:41:44 +0800

    A-TextUiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -7,9 +7,9 @@ then
 fi
 
 # delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
+if [ -e "./ACTUAL.txt" ]
 then
-    rm ACTUAL.TXT
+    rm ACTUAL.txt
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -19,15 +19,15 @@ then
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.txt
+java -classpath ../bin Socket < input.txt > ACTUAL.txt
 
 # convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# cp EXPECTED.txt EXPECTED-UNIX.txt
+# dos2unix ACTUAL.txt EXPECTED-UNIX.txt
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.txt EXPECTED.txt
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
