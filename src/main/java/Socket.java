d7e71613f5fcc510efd052100c325492adb0fd0d
Song Yiyang <E1456773@u.nus.edu>
2026-01-20 17:35:50 +0800

    Add Level-2, refactor code

--- src/main/java/Socket.java
+++ src/main/java/Socket.java
@@ -1,29 +1,40 @@
 import java.util.Scanner;
 
 public class Socket {
-    private static final String hline = "____________________________________________________________";
-    private static final String logo = "Hello! I'm Socket\n\tWhat can I do for you?";
-    private static final String goodbye = "Bye. Hope to see you again soon!";
+    private static final String HLINE = "____________________________________________________________";
+    private static final String LOGO = "Hello! I'm Socket\n\tWhat can I do for you?";
+    private static final String GOODBYE = "Bye. Hope to see you again soon!";
 
-    private static void printText(String text) {
-        System.out.println('\t' + Socket.hline);
-        System.out.println('\t' + text);
-        System.out.println('\t' + Socket.hline);
-        System.out.println();
-    }
+    private static int numTasks = 0;
+    private static String[] tasks = new String[100];
 
     public static void main(String[] args) {
-        Socket.printText(Socket.logo);
+        System.out.println('\t' + Socket.HLINE);
+        System.out.println('\t' + Socket.LOGO);
+        System.out.println('\t' + Socket.HLINE);
+        System.out.println();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
+
+            System.out.println('\t' + Socket.HLINE);
             if (input.equals("bye")) {
-                Socket.printText(Socket.goodbye);
+                System.out.println('\t' + Socket.GOODBYE);
+                System.out.println('\t' + Socket.HLINE);
+                System.out.println();
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < Socket.numTasks; i++) {
+                    System.out.println('\t' + String.valueOf(i+1) + ". " + Socket.tasks[i]);
+                }
             } else {
-                Socket.printText(input);
+                Socket.tasks[Socket.numTasks++] = input;
+                System.out.println("\tadded: " + input);
             }
+
+            System.out.println('\t' + Socket.HLINE);
+            System.out.println();
         }
     }
 }
