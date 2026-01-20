edb43324fe5f0ccccfd5c75b7e7238fe46d48bdb
Song Yiyang <E1456773@u.nus.edu>
2026-01-20 17:22:09 +0800

    Add Level-1

--- src/main/java/Socket.java
+++ src/main/java/Socket.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Socket {
+    private static final String hline = "____________________________________________________________";
+    private static final String logo = "Hello! I'm Socket\n\tWhat can I do for you?";
+    private static final String goodbye = "Bye. Hope to see you again soon!";
+
+    private static void printText(String text) {
+        System.out.println('\t' + Socket.hline);
+        System.out.println('\t' + text);
+        System.out.println('\t' + Socket.hline);
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        String logo = """
-                ____________________________________________________________
-                 Hello! I'm Socket
-                 What can I do for you?
-                ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                ____________________________________________________________
-                """;
-        System.out.println(logo);
+        Socket.printText(Socket.logo);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                Socket.printText(Socket.goodbye);
+                break;
+            } else {
+                Socket.printText(input);
+            }
+        }
     }
 }
